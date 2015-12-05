618b222cb7f37f40486fdc2135c7e903c88c1f86
Thu Tran <thu.tran10@yahoo.com>
2015-12-05 14:35:24 -0500

    selection by MapRduce: added  and ReduceSelectionClass

--- src/CitiesAndCountriesStatistics.java
+++ src/CitiesAndCountriesStatistics.java
@@ -1,9 +1,27 @@
-import java.io.IOException;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 /* map(<key, value>){ // data preparation
  *  // filter out field of interests
@@ -21,26 +39,74 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  * // iterate throught the list and pick out value based on condition 
  * */
 
-public class CitiesAndCountriesStatistics {
-
-	// Entry point
-	public static void main(String[] args) throws IOException {
-		if(args.length !=2){
-			System.err.println("Please provide an <input path> and <output path>");
-			System.exit(-1);
+public class CitiesAndCountriesStatistics extends Configured implements Tool{
+	
+	public static class MapSelectionClass extends MapReduceBase 
+	implements Mapper<LongWritable, Text, Text, Text>{
+		
+	    private Text city = new Text(); // type of output key
+	    private Text population = new Text(); // type of output value
+	    
+	    @Override
+	    public void map(LongWritable key, Text value, 
+	    				OutputCollector<Text, Text> output, Reporter reporter) throws IOException{
+	
+	    	String[] tuple = value.toString().split(",");
+	    	
+	    	String cityName = tuple[1];
+	    	String populationSize = tuple[4];
+	    	
+	    	Integer PopulationSizeInt = Integer.parseInt(populationSize);
+	    	
+	    	if(PopulationSizeInt > 30000){ 
+	    		city.set(cityName);
+	    		population.set(PopulationSizeInt.toString());
+	    		System.err.println("city: " + cityName + "\t population: " + populationSize.toString());
+	    		output.collect(city, population); // emit key-value pair
+	    	}
+	    }
+	}
+	
+	public static class ReduceSelectionClass extends MapReduceBase implements Reducer<Text, Text, Text, Text>{
+		
+		@Override
+		public void reduce(Text key, Iterator<Text> values, 
+						   OutputCollector<Text, Text> output, Reporter reporter) throws IOException{
+	    	while(values.hasNext()){
+	    		output.collect(key, values.next()); // create a pair <city, population>
+	    	}
 		}
+	    }
+
+	  @Override
+	  public int run(String[] args) throws IOException
+	  {
+	    return 0;
+	  }	
+	
+	// Entry point
+	public static void main(String[] args) throws Exception {
 		
 		// 1. Create a job name "Cities and countries statistics"
-		Job job = new Job();
-		job.setJarByClass(CitiesAndCountriesStatistics.class); // pass the class so Hadoop will locate the relevant JAR file by looking for the JAR file containing this class 
-		job.setJobName("Cities and countries statistics");
+		 JobConf job = new JobConf(CitiesAndCountriesStatistics.class); // pass the class so Hadoop will locate the relevant JAR file by looking for the JAR file containing this class 
+		 job.setJobName("Cities and countries statistics");
 		
+		 job.setOutputKeyClass(Text.class);
+		 job.setOutputValueClass(Text.class);
+
+		 job.setMapperClass(MapSelectionClass.class);
+		 job.setReducerClass(ReduceSelectionClass.class);
+
+		 job.setInputFormat(TextInputFormat.class);
+		 job.setOutputFormat(TextOutputFormat.class);
+		 
 		// 2. Specify input paths
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		
+		FileInputFormat.setInputPaths(job, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectInput/city.txt"));
+	
 		// 3. Specify output paths
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectOutput"));
+	
 		
-		
+		JobClient.runJob(job);		
 	}
 }
