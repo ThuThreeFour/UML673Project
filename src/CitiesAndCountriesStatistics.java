02bf135af43d1c541abf58e6d95156a8c3d478a2
Thu Tran <thu.tran10@yahoo.com>
2015-12-07 20:10:50 -0500

    added Map and Reduce classes for section 3.3 Computing Natural Join by MapReduce

--- src/CitiesAndCountriesStatistics.java
+++ src/CitiesAndCountriesStatistics.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
@@ -90,6 +92,58 @@ public class CitiesAndCountriesStatistics extends Configured implements Tool{
 	    }
 	}
 	
+	public static class MapNaturalJoinClass1 extends MapReduceBase 
+	implements Mapper<LongWritable, Text, Text, Text>{
+		
+	    private Text countryCode = new Text(); // type of output key
+	    private Text language = new Text(); // type of output value
+	    
+	    @Override
+	    public void map(LongWritable key, Text value, 
+	    				OutputCollector<Text, Text> output, Reporter reporter) throws IOException{
+	
+	    	String[] tuple = value.toString().split(",");
+	    	
+	    	String countrycode = tuple[0];
+	    	String languageSpoken = tuple[1];
+	    	String isOfficial = tuple[2];
+	    	
+	    	if(languageSpoken.equals("English") && isOfficial.equals("T") ){ 
+	    		countryCode.set(countrycode);
+	    		language.set(languageSpoken);
+	    		System.err.println("country code: " + countrycode + "\t language: " + languageSpoken);
+	    		output.collect(countryCode, language); // emit key-value pair
+	    	}
+	    }
+	}
+	
+	public static class MapNaturalJoinClass2 extends MapReduceBase 
+	implements Mapper<LongWritable, Text, Text, Text>{
+		
+	    private Text CountryCode = new Text(); // type of output key
+	    private Text CountryName = new Text(); // type of output value
+	    
+	    @Override
+	    public void map(LongWritable key, Text value, 
+	    				OutputCollector<Text, Text> output, Reporter reporter) throws IOException{
+	
+	    	String[] tuple = value.toString().split(",");
+	    	
+	    	String countrycode = tuple[0];
+	    	String countryname = tuple[1];
+	    	
+
+	    	CountryCode.set(countrycode);
+	    	CountryName.set(countryname);
+	    	System.err.println("country code: " + countrycode + "\t country name: " + countryname);
+	    	output.collect(CountryCode, CountryName); // emit key-value pair
+
+	    }
+	}
+	
+	
+
+	
 	public static class MapAggregationClass extends MapReduceBase 
 	implements Mapper<LongWritable, Text, Text, IntWritable>{
 		
@@ -106,7 +160,7 @@ public class CitiesAndCountriesStatistics extends Configured implements Tool{
 	    	String districtName = tuple[3];
 
 	    	district.set(districtName);
-	    	System.err.println("district: " + cityName + "\tcity: " + cityName + "\tcount: " + one);
+	    	//System.err.println("district: " + cityName + "\tcity: " + cityName + "\tcount: " + one);
 	    	output.collect(district, one); // emit key-value pair
 	    }
 	}
@@ -131,6 +185,31 @@ public class CitiesAndCountriesStatistics extends Configured implements Tool{
 			while(values.hasNext()){
 				output.collect(key, values.next()); // create a pair <city, population>
 			}
+		}
+	}
+
+	public static class ReduceNaturalJoinClass extends MapReduceBase implements Reducer<Text, Text, Text, Text>{
+		
+		@Override
+		public void reduce(Text key, Iterator<Text> values, 
+				OutputCollector<Text, Text> output, Reporter reporter) throws IOException{
+			String[] currentVals = new String[2];
+			int index = 0;
+			System.out.println(key.toString());
+			while(values.hasNext()){
+				currentVals[index] = values.next().toString();
+				System.out.println("index:" + index + "\tvalue: " + currentVals[index]);
+				index++;
+			}
+			
+			if(index == 2){
+				if(currentVals[0].equals("English")){
+					output.collect(key, new Text(currentVals[1] + "\t" + currentVals[0]));
+				} else {
+					output.collect(key, new Text(currentVals[0] + "\t" + currentVals[1]));
+				}	
+			}
+			
 		}
 	}
 	
@@ -173,21 +252,34 @@ public class CitiesAndCountriesStatistics extends Configured implements Tool{
 		  JobConf projectionJob = new JobConf(CitiesAndCountriesStatistics.class); // pass the class so Hadoop will locate the relevant JAR file by looking for the JAR file containing this class 
 		  projectionJob.setJobName("Computing Projection by MapReduce");
 		  
+		  // 3.2.1 Create a job name "Computing Projection by MapReduce"
+		  JobConf naturalJoinJob = new JobConf(CitiesAndCountriesStatistics.class); // pass the class so Hadoop will locate the relevant JAR file by looking for the JAR file containing this class 
+		  naturalJoinJob.setJobName("Computing Natural Join by MapReduce");
+		  
 		  // 3.4 Create a job name Aggregation by MapReduce
 		  JobConf aggregationJob = new JobConf(CitiesAndCountriesStatistics.class); // pass the class so Hadoop will locate the relevant JAR file by looking for the JAR file containing this class 
-		  aggregationJob.setJobName("Aggregation by MapReduce: Find how many cities each district has");
+		  aggregationJob.setJobName("Aggregation by MapReduce");
 		  
 		  // 3.1.2
 		  job.setOutputKeyClass(Text.class);
 		  job.setOutputValueClass(Text.class);
 		  aggregationJob.setOutputKeyClass(Text.class);
 		  aggregationJob.setOutputValueClass(IntWritable.class);
+		  naturalJoinJob.setOutputKeyClass(Text.class);
+		  naturalJoinJob.setOutputValueClass(Text.class);
 		  projectionJob.setOutputKeyClass(Text.class);
 		  projectionJob.setOutputValueClass(Text.class);
 		  
 		  // 3.1.3
 		  job.setMapperClass(MapSelectionClass.class);
 		  job.setReducerClass(ReduceSelectionClass.class);
+		  projectionJob.setMapperClass(MapProjectionClass.class);
+		  projectionJob.setReducerClass(ReduceProjectionClass.class);
+		  naturalJoinJob.setNumMapTasks(2);
+		  naturalJoinJob.setMapperClass(MapNaturalJoinClass1.class);
+		  naturalJoinJob.setMapperClass(MapNaturalJoinClass2.class);
+		  //projectionJob.setNumReduceTasks(1);
+		  naturalJoinJob.setReducerClass(ReduceNaturalJoinClass.class);
 		  projectionJob.setMapperClass(MapProjectionClass.class);
 		  projectionJob.setReducerClass(ReduceProjectionClass.class);
 		  aggregationJob.setMapperClass(MapAggregationClass.class);
@@ -199,22 +291,27 @@ public class CitiesAndCountriesStatistics extends Configured implements Tool{
 		  job.setOutputFormat(TextOutputFormat.class);
 		  projectionJob.setInputFormat(TextInputFormat.class);
 		  projectionJob.setOutputFormat(TextOutputFormat.class);
+		  naturalJoinJob.setOutputFormat(TextOutputFormat.class);
 		  aggregationJob.setInputFormat(TextInputFormat.class);
 		  aggregationJob.setOutputFormat(TextOutputFormat.class);
 
 		  // 3.1.5 Specify input paths
 		  FileInputFormat.setInputPaths(job, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectInput/city.txt"));	
 		  FileInputFormat.setInputPaths(projectionJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectInput/city.txt"));	
+		  MultipleInputs.addInputPath(naturalJoinJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectInput/countrylanguage.txt"),TextInputFormat.class, MapNaturalJoinClass1.class); 
+		  MultipleInputs.addInputPath(naturalJoinJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectInput/country.txt"),TextInputFormat.class, MapNaturalJoinClass2.class); 
 		  // 3.4.5
 		  FileInputFormat.setInputPaths(aggregationJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectInput/city.txt"));	
 		  
 		  // 3.1.6 Specify output paths
 		  FileOutputFormat.setOutputPath(job, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectOutput1"));
 		  FileOutputFormat.setOutputPath(projectionJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectOutput2"));
+		  FileOutputFormat.setOutputPath(naturalJoinJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectOutput3"));
 		  FileOutputFormat.setOutputPath(aggregationJob, new Path("hdfs://localhost:9000/user/hadoop/UML673ProjectOutput4"));
 
 		  JobClient.runJob(job);
 		  JobClient.runJob(projectionJob);
+		  JobClient.runJob(naturalJoinJob);
 		  JobClient.runJob(aggregationJob);
 	  }
 }
